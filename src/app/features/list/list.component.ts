c1e4eb82046d55cff6bc8318d504c483e268fbe4
gVieiraX <gabriel_silvarosa12@hotmail.com>
2024-09-02 17:49:13 -0300

    refactor: refatora listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-list',
@@ -11,12 +12,11 @@ import { Component, inject } from '@angular/core';
 export class ListComponent {
   products: any[] = []; 
 
-  httpClient = inject(HttpClient);
+  productsService = inject(ProductsService);
 
   ngOnInit(){
-    this.httpClient.get<any>('/api/products').subscribe((products) =>{
+    this.productsService.getAll().subscribe((products) =>{
       this.products = products;
     });
   }
-
 }
