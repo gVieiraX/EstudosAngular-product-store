cf4d13a2affd4712f12d6a4afc12ae3ade8b8740
gVieiraX <gabriel_silvarosa12@hotmail.com>
2024-10-01 15:11:36 -0300

    refactor: melhora código de ListComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,15 @@ import { CreateComponent } from './features/create/create.component';
 import { Observable } from 'rxjs';
 import { ProductsService } from './shared/services/products.service';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+    {
 path: '',
+resolve:{
+    products:() =>{
+        const productsService = inject(ProductsService)
+        return productsService.getAll();
+    }
+},
     component: ListComponent
 },
 {
